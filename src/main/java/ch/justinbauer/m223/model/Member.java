d964d34a5b66f0b360a4acf543dda2bac71c7d9c
justinnickbauer <jbpemo@gmail.com>
2022-11-03 13:43:49 +0000

    feat: add Member Model

---
+++ src/main/java/ch/justinbauer/m223/model/Member.java
@@ -0,0 +1,90 @@
+package ch.justinbauer.m223.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Entity
+public class Member {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Schema(readOnly = true)
+  private Long id;
+  
+  @Column(nullable = false )
+  private String firstname;
+
+  @Column(nullable = false )
+  private String lastname;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private boolean isBlocked;
+
+  @ManyToOne
+  @JoinColumn(name="role_id", nullable=false)
+  private Role role;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public Role getRole() {
+    return role;
+  }
+
+  public void setRole(Role role) {
+    this.role = role;
+  }
+
+  
+}
