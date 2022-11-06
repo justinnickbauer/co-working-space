e7360fc1e8a16f9cb7f26149c8c08b9c62152238
justinnickbauer <jbpemo@gmail.com>
2022-11-06 13:58:32 +0000

    feat: add authcontroller

---
+++ src/main/java/ch/justinbauer/m223/controller/AuthController.java
@@ -0,0 +1,33 @@
+package ch.justinbauer.m223.controller;
+
+import javax.annotation.security.PermitAll;
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import ch.justinbauer.m223.model.dto.CredentialDto;
+import ch.justinbauer.m223.service.AuthService;
+
+@Path("/auth")
+@Tag(name = "Authentication", description = "Handling of Authentications")
+@PermitAll
+public class AuthController {
+    
+    @Inject
+    AuthService authService;
+  
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Authenticate a user.", description = "Returns a token upon successful authentication.")
+    public Response create(@Valid CredentialDto credentialdto) {
+      return this.authService.signin(credentialdto);
+    }
+
+}
